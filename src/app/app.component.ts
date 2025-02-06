e8da93a4ef2ec622c818c98aef9769e255a4c2ef
AndrewToledo <toledoandrew3@gmail.com>
2025-02-06 16:46:19 -0300

    Adicionando arquivos do projeto indexa Agenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { SeparadorComponent } from './separador/separador.component';
+import { ContatoComponent } from './contato/contato.component';
 
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+import output from './output.json';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'agenda';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwyz';
+  contatos: Contato[] = output;
+
+  filtrarContatoPorLetraInicial(letra: string): Contato[] {
+    return this.contatos.filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }
